ef487cbbe7f08a6fbbb856c66c3b81c5b1bcc9a0
KOKI OGATA <koki.ogata@leonisand.co>
2016-02-28 22:44:37 +0900

    Modify landing Page

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,4 +10,5 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
+Rails.application.config.assets.precompile += %w( rails.png )
 Rails.application.config.assets.precompile += %w( top_image.png )
