d3bbb8a3d28329b92f5709f3a8c60721c66d093c
KOKI OGATA <koki.ogata@leonisand.co>
2016-03-04 00:47:32 +0900

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,9 +14,15 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
-  test "shold get about" do
+  test "should get about" do
     get :about
     assert_response :success
     assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 end
