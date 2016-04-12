a5eaed49c8d0c9d7b3e6976622e55c21f5215f23
KOKI OGATA <koki.ogata@leonisand.co>
2016-04-13 00:29:10 +0900

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -56,4 +56,10 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a"*5
     assert_not @user.valid?
   end
+
+  test "password should be downcased"
+    mixed_case_email = "Foo@EjboiL.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
 end
