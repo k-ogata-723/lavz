522306fb4bf191fbd232e368b6f8dc9414d956f0
KOKI OGATA <koki.ogata@leonisand.co>
2017-04-09 00:57:56 +0900

    Start add protocol modify

--- app/assets/javascripts/components/micropost_item.js.jsx
+++ app/assets/javascripts/components/micropost_item.js.jsx
@@ -9,6 +9,7 @@ var MessageItem = React.createClass ({
   },
 
   render: function() {
+    // console.log("this.props.message", this.props.message.id);
     return (
       <div className="message">
         <label className="messageUser" onDoubleClick={this.handleEdit}>{this.props.message.content}</label>
--- app/assets/javascripts/components/protocol_item.js.jsx
+++ app/assets/javascripts/components/protocol_item.js.jsx
@@ -1,14 +1,16 @@
 var ProtocolItem = React.createClass ({
   render: function() {
-    // console.log("protocol array length", this.props.protocol.length);
+    // デバッグ用に表示
     console.log("protocol array", this.props.protocol);
     console.log("protocol array [0]", this.props.protocol[0]);
-    // for ( i = 0; i < this.props.protocol.length; i++) {
+    console.log("protocol の要素（procedure）", this.props.protocol[0].procedure);
+    // for文の中で、protocolオブジェクトのprocedure要素を順に表示
+    for ( i = 0; i < this.props.protocol.length; i++) {
       return (
         <div className="protocol">
-          <h2 className="protocolUser">{this.props.protocol}</h2>
+          <h2 className="protocolUser">{this.props.protocol[i].procedure}</h2>
         </div>
       );
-    // }
+    }
   }
 });
